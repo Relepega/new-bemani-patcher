7f7a7a392ededa4bc4dca624ec73220f17ad4e20
Relepega <33182302+Relepega@users.noreply.github.com>
2021-05-24 00:26:34 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -3,9 +3,11 @@
 /**
  * @type {import('@vue/cli-service').ProjectOptions}
  */
+/*
 module.exports = {
 	publicPath: process.env.NODE_ENV === 'production' ? '/BemaniPatcher' : '/',
 	chainWebpack: (config) => {
 		config.resolve.symlinks(false)
 	},
 }
+*/
